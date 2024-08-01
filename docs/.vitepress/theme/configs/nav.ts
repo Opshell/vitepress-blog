86cd1fff5fd077869fd630aea81978792ca546a0
opshell <11209046@gs.ncku.edu.tw>
2024-08-01 14:07:06 +0800

    fix：link, add：socialLink svg

--- docs/.vitepress/theme/configs/nav.ts
+++ docs/.vitepress/theme/configs/nav.ts
@@ -4,27 +4,27 @@ import { DefaultTheme } from 'vitepress';
 export default [
     {
         text: 'About Ops',
-        link: '/pages/about-ops'
+        link: '/about-ops'
     },
     {
         text: 'Article',
         items: [
             {
                 text: "Code Sea",
-                link: '/pages/article/code-sea/list'
+                link: '/article/code-sea/list'
             },
             {
                 text: "Life's Mumurs",
-                link: '/pages/article/life-mumurs/list'
+                link: '/article/life-mumurs/list'
             },
             {
                 text: "Miscellaneous Attempts",
-                link: '/pages/article/attempts/list'
+                link: '/article/attempts/list'
             },
         ]
     },
     {
         text: 'Resume',
-        link: '/pages/resume'
+        link: '/resume'
     }
 ] as DefaultTheme.NavItem[];
--- docs/.vitepress/theme/configs/sidebar.ts
+++ docs/.vitepress/theme/configs/sidebar.ts
@@ -2,44 +2,44 @@
 import { DefaultTheme } from 'vitepress';
 
 export default {
-    '/pages/article/code-sea/': [
+    '/article/code-sea/': [
         {
             text: 'Vue',
             items: [
-                { text: 'Why Vue', link: '/pages/article/code-sea/vue/1-Why-Vue' },
-                { text: 'Components', link: '/pages/article/code-sea/vue/2-Components' }
+                { text: 'Why Vue', link: '/article/code-sea/vue/1-Why-Vue' },
+                { text: 'Components', link: '/article/code-sea/vue/2-Components' }
             ]
         },
         {
             text: 'TypeScript',
             items: [
-                { text: "Who's TypeScript", link: "/pages/article/code-sea/typescript/1-Who's-TypeScript" },
-                { text: 'Just Begin', link: '/pages/article/code-sea/typescript/2-Just-Begin' }
+                { text: "Who's TypeScript", link: "/article/code-sea/typescript/1-Who's-TypeScript" },
+                { text: 'Just Begin', link: '/article/code-sea/typescript/2-Just-Begin' }
             ]
         },
         {
             text: 'CSS',
             items: [
-                { text: 'Meet Css', link: '/pages/article/code-sea/vue/1-why-vue' },
-                { text: 'Runtime API Examples', link: '/pages/article/code-sea/vue/1-why-vue' }
+                { text: 'Meet Css', link: '/article/code-sea/vue/1-why-vue' },
+                { text: 'Runtime API Examples', link: '/article/code-sea/vue/1-why-vue' }
             ]
         },
     ],
-    '/pages/article/life-mumurs/': [
+    '/article/life-mumurs/': [
         {
             text: 'Life Mumurs',
-            item: [
-                { text: 'Life is it', link: '/pages/article-list' },
-                { text: 'Runtime API Examples', link: '/pages/api-examples' }
+            items: [
+                { text: 'Life is it', link: '/article-list' },
+                { text: 'Runtime API Examples', link: '/api-examples' }
             ]
         }
     ],
-    '/pages/article/attempts/': [
+    '/article/attempts/': [
         {
             text: 'Miscellaneous Attempts',
-            item: [
-                { text: 'Article List', link: '/pages/article-list' },
-                { text: 'Runtime API Examples', link: '/pages/api-examples' }
+            items: [
+                { text: 'Article List', link: '/article-list' },
+                { text: 'Runtime API Examples', link: '/api-examples' }
             ]
         }
     ],
